56215e89ee477d81f782c8b35cd4ea56f7be11df
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 21:42:32 +0530

    home_screen.dart: SharedPrefService.clearOnboardingStatus add to onPress().

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fourth_onboarding_screen/core/services/shared_pref_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +10,10 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: Text("Home Screen")),
       body: SafeArea(
         child: Center(
-          child: ElevatedButton(onPressed: () {}, child: Text("Clear State")),
+          child: ElevatedButton(
+            onPressed: SharedPrefService.clearOnboardingStatus,
+            child: Text("Clear State"),
+          ),
         ),
       ),
     );
