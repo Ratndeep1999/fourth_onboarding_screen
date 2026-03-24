a9b8b71d6c694181358a1e6b4f01a19351e8300a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 19:56:58 +0530

    onboarding_data.dart: onboarding data add to List type variable onboardingData.

---
+++ lib/onboarding/data/onboarding_data.dart
@@ -0,0 +1,26 @@
+import 'package:fourth_onboarding_screen/onboarding/model/onboarding_model.dart';
+import 'package:fourth_onboarding_screen/utils/constants/app_icons.dart';
+import 'package:fourth_onboarding_screen/utils/constants/app_texts.dart';
+
+final List<OnboardingModel> onboardingData = [
+  OnboardingModel(
+    icon: AppIcons.kOnboarding_icon1,
+    title: AppTexts.kOnBoardingTitle1,
+    desc: AppTexts.kOnBoardingDesc1,
+  ),
+  OnboardingModel(
+    icon: AppIcons.kOnboarding_icon2,
+    title: AppTexts.kOnBoardingTitle2,
+    desc: AppTexts.kOnBoardingDesc2,
+  ),
+  OnboardingModel(
+    icon: AppIcons.kOnboarding_icon3,
+    title: AppTexts.kOnBoardingTitle3,
+    desc: AppTexts.kOnBoardingDesc3,
+  ),
+  OnboardingModel(
+    icon: AppIcons.kOnboarding_icon4,
+    title: AppTexts.kOnBoardingTitle4,
+    desc: AppTexts.kOnBoardingDesc4,
+  ),
+];
