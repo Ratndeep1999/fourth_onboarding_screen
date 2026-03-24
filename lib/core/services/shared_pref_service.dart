5d9d73e2b8f090564b6905bfd6e5ae258ce3c887
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 21:41:26 +0530

    shared_pref_service.dart: setOnboardingStatus(), getOnboardingStatus() and clearOnboardingStatus() methods add.

--- lib/core/services/shared_pref_service.dart
+++ lib/core/services/shared_pref_service.dart
@@ -1,3 +1,5 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class SharedPrefService {
   /// Singleton class
   SharedPrefService._internal();
@@ -5,4 +7,25 @@ class SharedPrefService {
   static final SharedPrefService _instance = SharedPrefService._internal();
 
   static SharedPrefService get instance => _instance;
+
+  /// Onboarding key
+  static const String _onboardingKey = "onboarding_status";
+
+  /// Set Onboarding Status
+  static Future<void> setOnboardingStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_onboardingKey, true);
+  }
+
+  /// Get Onboarding Status
+  static Future<bool> getOnboardingStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_onboardingKey) ?? false;
+  }
+
+  /// Clear Onboarding Status
+  static Future<void> clearOnboardingStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_onboardingKey);
+  }
 }
