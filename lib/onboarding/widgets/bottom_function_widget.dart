4f4948d5fd7f4ce0d5f29a3cf4af7e10b5d56c63
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 20:52:00 +0530

    bottom_function_widget.dart: BottomFunctionWidget() refactor and fields add.

---
+++ lib/onboarding/widgets/bottom_function_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:fourth_onboarding_screen/onboarding/data/onboarding_data.dart';
+
+class BottomFunctionWidget extends StatelessWidget {
+  const BottomFunctionWidget({
+    super.key,
+    required int pageIndex,
+    required this.onNextPress,
+    required this.onDotTap,
+  }) : _pageIndex = pageIndex;
+
+  final int _pageIndex;
+  final VoidCallback onNextPress;
+  final Function(int) onDotTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        /// Dot Indicator
+        Row(
+          children: List.generate(
+            onboardingData.length,
+
+            /// Single Dot
+            (index) => InkWell(
+              onTap: onDotTap(index),
+              child: AnimatedContainer(
+                height: (_pageIndex == index) ? 40 : 16,
+                width: 5,
+                margin: EdgeInsets.symmetric(horizontal: 8.0),
+                decoration: BoxDecoration(
+                  color: (_pageIndex == index)
+                      ? Colors.blue
+                      : Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                duration: Duration(milliseconds: 300),
+              ),
+            ),
+          ),
+        ),
+        Spacer(),
+
+        /// Next Button
+        FilledButton(
+          onPressed: onNextPress,
+          style: FilledButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: CircleBorder(),
+            padding: EdgeInsets.all(20),
+          ),
+          child: Icon(Icons.arrow_forward, size: 24),
+        ),
+      ],
+    );
+  }
+}
