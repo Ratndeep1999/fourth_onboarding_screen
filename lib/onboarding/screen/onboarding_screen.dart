eff4e0c97b9dcfa43a6a5d5d43b3a7b63fa2f0a6
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 15:58:51 +0530

    onboarding_screen.dart: title and description widget add.

--- lib/onboarding/screen/onboarding_screen.dart
+++ lib/onboarding/screen/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fourth_onboarding_screen/utils/constants/app_icons.dart';
+import 'package:fourth_onboarding_screen/utils/constants/app_texts.dart';
 import 'package:lottie/lottie.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -12,7 +14,44 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
-      body: Center(child: Lottie.asset("assets/onboarding/onboarding_4.json")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /// Lottie Image
+            Lottie.asset(AppIcons.kOnboarding_icon1, height: 300, width: 300),
+            const SizedBox(height: 30),
+
+            /// Title
+            Text(
+              AppTexts.kOnBoardingTitle1,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 28,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            /// Description
+            Text(
+              AppTexts.kOnBoardingDesc1,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            //
+            Row(),
+          ],
+        ),
+      ),
     );
   }
 }
